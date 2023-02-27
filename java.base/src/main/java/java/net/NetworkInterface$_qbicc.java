298899c9df69144c0afe123c6c4da1676bd3997c
David Grove <groved@us.ibm.com>
2023-02-27 16:43:19 -0500

    more debug

--- java.base/src/main/java/java/net/NetworkInterface$_qbicc.java
+++ java.base/src/main/java/java/net/NetworkInterface$_qbicc.java
@@ -99,7 +99,9 @@ class NetworkInterface$_qbicc {
     static netif addif(c_int sock, const_char_ptr if_name, netif ifs,
                        ptr<struct_sockaddr> ifr_addrP, ptr<struct_sockaddr> ifr_broadaddrP,
                        c_int family, c_short prefix) {
-        debug("entering addif", ifr_broadaddrP.cast());
+        if (!ifr_broadaddrP.isNull()) {
+            debug("entering addif", ifr_broadaddrP.cast());
+        }
         netif currif = ifs;
         c_char[] name = new c_char[IF_NAMESIZE.intValue()];
         c_char[] vname = new c_char[IF_NAMESIZE.intValue()];
